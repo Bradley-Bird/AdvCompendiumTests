daf96017f327935f4127e152ba738e313f9d8159
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-26 11:49:28 -0700

    pokemon names on screeen

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,16 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPokemon } from '../services/pokemon';
 
 function Main() {
+  const [pokemon, setPokemon] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetchPokemon();
-      console.log(resp);
+      //   console.log(resp);
+      setPokemon(resp);
     };
     fetchData();
   }, []);
 
-  return <div>Main</div>;
+  return (
+    <>
+      {pokemon.map((pokemon) => (
+        <li key={pokemon.id}>{pokemon.pokemon}</li>
+      ))}
+    </>
+  );
 }
 
 export default Main;
